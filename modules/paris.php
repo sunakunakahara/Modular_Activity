bf31c2b8710b011451804a69ebfbb54e040378ed
Camille <roxas,camille@15.gmail.com>
2025-10-14 20:19:32 +0800

    first commit

---
+++ modules/paris.php
@@ -0,0 +1,4 @@
+
+    <h1>PARIS</h1>
+    <p>Paris is the capital city of France. It is one of the most populous cities in Europe, with a metropolitan area of over 11 million inhabitants.</p>
+    <p>Situated on the River Seine, Paris has been a major center of art, fashion, and culture for centuries, often called "The City of Light."</p>
---
+++ modules/tokyo.php
@@ -0,0 +1,4 @@
+
+    <h1>TOKYO</h1>
+    <p>Tokyo is the capital city of Japan. It is the most populous metropolitan area in the world, home to more than 37 million people.</p>
+    <p>Located on the eastern coast of Honshu, Tokyo blends modern skyscrapers with traditional temples, and is a global hub for technology, culture, and finance.</p>
